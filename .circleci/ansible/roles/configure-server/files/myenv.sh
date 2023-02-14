cf36da64f33f74fa61e7eef9ceaeaa54380f3b55
brian_kipkoech_tanui <briankipkoechtanui@address.com>
2023-02-15 02:11:45 +0300

    Configure Infrastructure

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,9 @@
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_HOST=database-1.cys59nkta9ig.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=postgres
+export TYPEORM_DATABASE=postgres
